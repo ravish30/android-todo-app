52f247e549ff0f16231099ed7b8909ba6e67b079
Ravish Goyal <goyalravish2001@gmail.com>
2024-01-03 21:09:01 +0530

    login functionality

--- app/src/main/java/com/example/todoapp/LoginActivity.java
+++ app/src/main/java/com/example/todoapp/LoginActivity.java
@@ -4,17 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button registerBtn;
+    private Button registerBtn, login_btn;
+    private ProgressBar progressBar;
+    private EditText email_ET, password_ET;
+    private String email, password;
+    UtilService utilService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         registerBtn = findViewById(R.id.registerBtn);
+        login_btn = findViewById(R.id.login_btn);
+        email_ET = findViewById(R.id.email_ET);
+        password_ET = findViewById(R.id.password_ET);
+        progressBar = findViewById(R.id.progress_bar);
+        utilService = new UtilService();
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +55,94 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                utilService.hideKeyboard(view, LoginActivity.this);
+                email = email_ET.getText().toString();
+                password = password_ET.getText().toString();
+                if(validate(view)) {
+                    loginUser();
+                }
+            }
+        });
+
+
+    }
+
+    private void loginUser() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        HashMap<String, String> Body = new HashMap<>();
+        Body.put("email", email);
+        Body.put("password", password);
+
+        String apiKey = "https://todo-api-android.vercel.app/auth/login";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, apiKey, new JSONObject(Body), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if(response.getBoolean("success")) {
+                        String message = response.getString("message");
+                        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                    else {
+                        String message = response.getString("message");
+                        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                    progressBar.setVisibility(View.GONE);
+                } catch (JSONException e) {
+                    progressBar.setVisibility(View.GONE);
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse response = error.networkResponse;
+                if(error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        JSONObject obj = new JSONObject(res);
+                        Toast.makeText(LoginActivity.this, obj.getString("message"), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                    catch(JSONException | UnsupportedEncodingException je) {
+                        progressBar.setVisibility(View.GONE);
+                        je.printStackTrace();
+                    }
+                }
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+
+        int socketTime = 3000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTime, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(policy);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public Boolean validate(View view) {
+        boolean isValid;
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            isValid = true;
+        }
+        else {
+            isValid = false;
+            utilService.showSnackBar(view, "All Fields are mandatory");
+        }
+
+        return isValid;
     }
 }
